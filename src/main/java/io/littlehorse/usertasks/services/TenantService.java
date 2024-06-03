151d092cb337ba5dfac7ec942516791e99346d5d
jhosep-marin <marinjhosep15@gmail.com>
2024-06-03 10:22:42 -0500

    chore: Upgrades LH sdk and refactors tenant existence verification
    Related-to: LH-394

--- src/main/java/io/littlehorse/usertasks/services/TenantService.java
+++ src/main/java/io/littlehorse/usertasks/services/TenantService.java
@@ -1,15 +1,12 @@
 package io.littlehorse.usertasks.services;
 
-import com.google.protobuf.ByteString;
 import io.littlehorse.sdk.common.proto.LittleHorseGrpc;
-import io.littlehorse.sdk.common.proto.SearchTenantRequest;
 import io.littlehorse.sdk.common.proto.TenantId;
-import io.littlehorse.sdk.common.proto.TenantIdList;
 import io.littlehorse.usertasks.exceptions.NotFoundException;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
-import java.util.function.Predicate;
+import java.util.Objects;
 
 @Service
 public class TenantService {
@@ -21,34 +18,10 @@ public class TenantService {
 
     //TODO: The way we ware verifying if a tenant is valid will probably change once we set production-ready OIDC configuration per tenant
     public boolean isValidTenant(@NonNull String tenantId) throws NotFoundException {
-        var firstSearchResults = lhClient.searchTenant(SearchTenantRequest.newBuilder().build());
-
-        if (firstSearchResults.getResultsList().isEmpty()) {
-            throw new NotFoundException("There are no tenants configured in server");
-        }
-
-        var foundTenantId = firstSearchResults.getResultsList().stream().anyMatch(matchesTenantId(tenantId));
-        var hasBookmark = firstSearchResults.hasBookmark();
-        var bookmark = firstSearchResults.getBookmark();
-
-        while (!foundTenantId && hasBookmark) {
-            TenantIdList subsequentSearchResults = getSubsequentSearchTenantResults(bookmark);
-
-            foundTenantId = subsequentSearchResults.getResultsList().stream().anyMatch(matchesTenantId(tenantId));
-            hasBookmark = subsequentSearchResults.hasBookmark();
-            bookmark = subsequentSearchResults.getBookmark();
-        }
-
-        return foundTenantId;
-    }
-
-    private Predicate<TenantId> matchesTenantId(String tenantId) {
-        return existingTenantId -> existingTenantId.getId().equalsIgnoreCase(tenantId);
-    }
-
-    private TenantIdList getSubsequentSearchTenantResults(ByteString bookmark) {
-        return lhClient.searchTenant(SearchTenantRequest.newBuilder()
-                .setBookmark(bookmark)
+        var tenant = lhClient.getTenant(TenantId.newBuilder()
+                .setId(tenantId)
                 .build());
+
+        return Objects.nonNull(tenant);
     }
 }
