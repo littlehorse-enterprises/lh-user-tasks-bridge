3dc48fa08362889c97bebbafac7ad63dbd160815
Saúl Piña <sauljabin@gmail.com>
2024-10-23 09:50:18 -0500

    refactor: automating local dev (#30)
    This is an extension of this:
    https://github.com/littlehorse-enterprises/lh-user-tasks-api/pull/29
    
    ---------
    
    Co-authored-by: Muhammad Qamar <muhammad@littlehorse.io>
    Co-authored-by: Saúl Piña <sauljabin@gmail.com>

--- README.md
+++ README.md
@@ -52,52 +52,21 @@ in `docker-compose.yaml`. You can run Keycloak, a local LH Server and the UserTa
   docker compose up -d
   ```
 
-If the `docker compose up` command ended successfully, then you should see 3 containers running:
+If the `docker compose up` command ended successfully, then you should see 4 containers running:
 
 - 1 for Keycloak
-- 1 for LittleHorse
-- 1 for user-tasks backend.
+- 1 for LittleHorse Standalone (LH Server and LH Dashboard)
+- 1 for user-tasks-api
+- 1 for user-tasks-ui
 
 After that, the UserTasks API should be available on http://localhost:8089
 
 Verify that Keycloak is up and running, and if that is the case, you can now access it through your browser
 on http://localhost:8888
 
-- Login as admin (username: **admin**, password: **admin**)
-- Switch to the **lh** realm
-- Go to the **Clients** section, look for the client **user-tasks-client** in the list of clients and click on it
-  to navigate to the **Settings** tab, scroll down until you reach the **Capability config** section, now disable the
-  _**Client authentication**_ property (unless you want to use Service Account roles) and keep default values for all
-  the other fields
-- Within the **Clients** section, click on your client id to see the details. Once you are seeing your client's details
-  navigate to the _Client Scopes_ tab and click on your client's scope. Once you have clicked on your client's scope,
-  you
-  will now see the mappers, and there you need to create one custom claim. To do that, click on _**Add mapper**_, and
-  select
-  _By configuration_. Now, from the list of mappings, choose **Hardcoded claim**. In the mapper details form you have to
-  set
-  **_allowed_tenant_** as the value in both fields _Name_ and _Token Claim Name_, and your tenant id MUST be the value
-  of
-  _Claim value_ field, keep the _Claim JSON Type_ as String and make sure that you switch on the _Add to access token_
-  property
-- Go to the **Users** section on the left sidebar, now you can create a user with username as `my-user`
-  (email and names are optional, so, feel free to fill them up or ignore them)
-- Go to the **Credentials** tab in **Users** section and add a password as `1234` (Make sure that you uncheck
-  the `Temporary` field)
-- Make sure that you assign the role `view-users` to all your users (ADMIN & NON-ADMIN) that will access UserTasks API
-  so
-  that they can query their own user details and see their groups. To do this you need to go to the **Users** section on
-  the
-  left sidebar, click on the user that you want to assign roles to, now go to the **_Role Mapping_** tab, click on
-  `Assign role`,
-  on the PopUp window that gets displayed select "Filter by clients", go through the list of available roles and select
-  the one named as `view-users` and click on **Assign**
-- In order to have an ADMIN user, you first need to create a role named as `lh-user-tasks-admin`. To create this role,
-  you need to go to the **Realm roles** section on the left sidebar, click on `Create role`, set **_lh-user-tasks-admin_
-  **
-  as _Role name_ and click on `Save` (Description is optional, so feel free to ignore it if you want)
-- Now that you have the UserTasks Admin role created, you can assign it to the users that you decide that are going to
-  have ADMIN privileges.
+Keycloak's admin credentials: 
+- username: **admin**
+- password: **admin**
 
 Also, you will need an access token to be able to be granted authorized access into user-tasks backend endpoints.
 In order to fetch said token, you can use the following cURL as is from the terminal, or you can import it
@@ -105,7 +74,7 @@ to your REST client tool of preference (Postman, Insomnia, Bruno, etc.)
 
 ```shell
 curl --request POST \
---url http://localhost:8888/realms/lh/protocol/openid-connect/token \
+--url http://localhost:8888/realms/default/protocol/openid-connect/token \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data client_id=user-tasks-client \
 --data client_secret= \
@@ -120,7 +89,7 @@ For example, you can hit the `/<tenant_id>/init` endpoint with the following cUR
 
 ```shell
 curl --request GET \
-  --url http://localhost:8089/lh/init \
+  --url http://localhost:8089/default/init \
   --header 'Authorization: Bearer replace-this-with-your-access-token'
 ```
 
@@ -136,7 +105,7 @@ Once your LH server is up and running, then you can create a tenant to work with
 the following command:
 
 ```shell
-lhctl put tenant here-you-put-your-tenant-identifier
+lhctl put tenant default
 ```
 
 #### Set the following environment variables to let the LH Client within UserTasks API know where the LH Server is located
@@ -144,7 +113,7 @@ lhctl put tenant here-you-put-your-tenant-identifier
  ```shell
   export LHC_API_HOST=localhost
   export LHC_API_PORT=2023
-  export LHC_TENANT_ID=here-you-put-your-tenant-identifier
+  export LHC_TENANT_ID=default
   ```
 
 This is just an example, use the values that match your LittleHorse config
--- local-dev/keycloak-configurer/configure-keycloak.sh
+++ local-dev/keycloak-configurer/configure-keycloak.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 configure_keycloak() {
     echo "Proceeding to create lh realm in keycloak and the client for user-tasks as sample OIDC provider"
@@ -15,22 +15,27 @@ configure_keycloak() {
         exit 1
     fi
 
-    REALM_NAME="lh"
+#  Here we set the Keycloak URL as a variable with a default value, so that it can be used in both local-env and when
+#  building the standalone docker image
+    KEYCLOAK_URL=${1:-"http://user-tasks-keycloak:8888"}
+    REALM_NAME="default"
     KEYCLOAK_ADMIN="admin"
     KEYCLOAK_ADMIN_PASSWORD="admin"
-    KEYCLOAK_PORT="8888"
     KEYCLOAK_CLIENT_ID="user-tasks-client"
+    KEYCLOAK_CLIENT_SECRET="any-secret"
 
     echo "Getting admin access token"
 
-    KEYCLOAK_ADMIN_ACCESS_TOKEN=$(http --ignore-stdin --form "http://user-tasks-keycloak:${KEYCLOAK_PORT}/realms/master/protocol/openid-connect/token" \
+#   Here we fetch Keycloak's admin access token. This access token will be used in all of the subsequent Http requests
+    KEYCLOAK_ADMIN_ACCESS_TOKEN=$(http --ignore-stdin --form "$KEYCLOAK_URL/realms/master/protocol/openid-connect/token" \
         client_id=admin-cli \
         username="$KEYCLOAK_ADMIN" \
         password="$KEYCLOAK_ADMIN_PASSWORD" \
         grant_type=password | jq -r ".access_token")
 
+#   Here we create the realm which will simulate a client's realm.
     echo "Creating realm"
-    http --ignore-stdin -q -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" POST "http://user-tasks-keycloak:${KEYCLOAK_PORT}/admin/realms" \
+    http --ignore-stdin -q -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" POST "$KEYCLOAK_URL/admin/realms" \
         id="$REALM_NAME" \
         realm="$REALM_NAME" \
         displayName="$REALM_NAME" \
@@ -41,21 +46,24 @@ configure_keycloak() {
         duplicateEmailsAllowed:=false \
         resetPasswordAllowed:=false \
         editUsernameAllowed:=false \
-        bruteForceProtected:=true
+        bruteForceProtected:=true \
+        accessTokenLifespan=86400 \
+        ssoSessionIdleTimeout=86400 \
+        ssoSessionMaxLifespan=90000
 
-    echo "Keycloak url: http://user-tasks-keycloak:${KEYCLOAK_PORT}"
-    echo "Keycloak admin username: ${KEYCLOAK_ADMIN}"
-    echo "Keycloak admin password: ${KEYCLOAK_ADMIN_PASSWORD}"
     echo "Realm '${REALM_NAME}' created"
 
+#   Here we create a Keycloak's client. This client will be used to customize some properties and also provide clientId and
+#   clientSecret required to perform some of the subsequent Http requests.
     echo "Creating Client"
-    http --ignore-stdin -q -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" POST "http://user-tasks-keycloak:${KEYCLOAK_PORT}/admin/realms/$REALM_NAME/clients" \
+    http --ignore-stdin -q -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" POST "$KEYCLOAK_URL/admin/realms/$REALM_NAME/clients" \
           id="$KEYCLOAK_CLIENT_ID"  \
           protocol="openid-connect" \
           clientId="$KEYCLOAK_CLIENT_ID" \
-          secret="${LHCC_KEYCLOAK_CLIENT_SECRET}" \
+          secret="${KEYCLOAK_CLIENT_SECRET}" \
           name="LH-Client" \
           description="LH-Client" \
+          redirectUris[]="*" \
           directAccessGrantsEnabled:=true \
           serviceAccountsEnabled:=true \
           standardFlowEnabled:=true \
@@ -65,25 +73,109 @@ configure_keycloak() {
           surrogateAuthRequired:=false \
           frontchannelLogout:=true
 
+   echo "Client successfully created"
 
-#  KEYCLOAK_CLIENT_SECRET=$(http --ignore-stdin -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" "http://user-tasks-keycloak:8888/admin/realms/lh/clients/$KEYCLOAK_CLIENT_ID/client-secret" | jq -r ".value")
+#  Here we create a custom claim that is also added to the accessToken. This custom claim is used by the lh-user-tasks-api
+#  to verify that the users accessing its endpoints are allowed to see resources from a given tenant.
+   echo "Creating tenant custom claim"
+   http --ignore-stdin -b -A bearer -a "${KEYCLOAK_ADMIN_ACCESS_TOKEN}" POST "$KEYCLOAK_URL/admin/realms/$REALM_NAME/clients/$KEYCLOAK_CLIENT_ID/protocol-mappers/models" \
+                 protocol=openid-connect \
+                 protocolMapper=oidc-hardcoded-claim-mapper \
+                 name=allowed_tenant \
+                 config[claim.name]=allowed_tenant \
+                 config[claim.value]=default \
+                 config[jsonType.label]=String \
+                 config[id.token.claim]:=true \
+                 config[access.token.claim]:=true \
+                 config[lightweight.claim]:=false \
+                 config[userinfo.token.claim]:=true \
+                 config[access.tokenResponse.claim]:=false \
+                 config[introspection.token.claim]:=true
 
-  SERVICE_ACCOUNT=$(http --ignore-stdin -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" "http://user-tasks-keycloak:8888/admin/realms/lh/clients/$KEYCLOAK_CLIENT_ID/service-account-user" | jq -r ".id")
+  SERVICE_ACCOUNT=$(http --ignore-stdin -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" "$KEYCLOAK_URL/admin/realms/$REALM_NAME/clients/$KEYCLOAK_CLIENT_ID/service-account-user" | jq -r ".id")
 
-  REAL_MANAGEMENT_CLIENT_ID=$(http --ignore-stdin -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" "http://user-tasks-keycloak:8888/admin/realms/lh/ui-ext/available-roles/users/$SERVICE_ACCOUNT?first=0&max=11&search=manage-user" | jq -r ".[0].clientId")
+  REAL_MANAGEMENT_CLIENT_ID=$(http --ignore-stdin -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" "$KEYCLOAK_URL/admin/realms/$REALM_NAME/ui-ext/available-roles/users/$SERVICE_ACCOUNT?first=0&max=11&search=manage-user" | jq -r ".[0].clientId")
 
-  MANAGE_USER_ROLE_ID=$(http --ignore-stdin -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" "http://user-tasks-keycloak:8888/admin/realms/lh/ui-ext/available-roles/users/$SERVICE_ACCOUNT?first=0&max=11&search=manage-user" | jq -r ".[0].id")
+  MANAGE_USER_ROLE_ID=$(http --ignore-stdin -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" "$KEYCLOAK_URL/admin/realms/$REALM_NAME/ui-ext/available-roles/users/$SERVICE_ACCOUNT?first=0&max=11&search=manage-user" | jq -r ".[0].id")
 
-  echo "Adding manage-users role to Client"
-   http --ignore-stdin -q -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" POST "http://user-tasks-keycloak:8888/admin/realms/lh/users/$SERVICE_ACCOUNT/role-mappings/clients/$REAL_MANAGEMENT_CLIENT_ID" \
+   echo "Adding manage-users role to Client"
+   http --ignore-stdin -q -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" POST "$KEYCLOAK_URL/admin/realms/$REALM_NAME/users/$SERVICE_ACCOUNT/role-mappings/clients/$REAL_MANAGEMENT_CLIENT_ID" \
     [0][description]='${role_manage-users}' \
     [0][id]="${MANAGE_USER_ROLE_ID}" \
     [0][name]="manage-users"
 
    echo "Keycloak Client Id '${KEYCLOAK_CLIENT_ID}' created"
-   echo "Keycloak Client Secret '${LHCC_KEYCLOAK_CLIENT_SECRET}' created"
+   echo "Keycloak Client Secret '${KEYCLOAK_CLIENT_SECRET}' created"
 
-#   TODO: It's probably a good idea to create the test user here as well
+#  Here we create a role that will later on be used to identify admin users.
+   echo "Creating UserTasks Admin Role"
+   http --ignore-stdin -q -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" POST "$KEYCLOAK_URL/admin/realms/$REALM_NAME/roles" \
+    name="lh-user-tasks-admin" \
+    description="This role is used to let UserTasks API know about which users are allowed to perform ADMIN actions."
+
+#  Here we create a user that will not have the admin role.
+   echo "Creating UserTasks NonAdmin User"
+   http --ignore-stdin -b -A bearer -a "${KEYCLOAK_ADMIN_ACCESS_TOKEN}" POST "$KEYCLOAK_URL/admin/realms/$REALM_NAME/users" \
+           emailVerified:=true \
+           username="my-user" \
+           email="someemailaddress@somedomain.com" \
+           firstName="local" \
+           lastName="dev" \
+           enabled:=true \
+           credentials[0][type]="password" \
+           credentials[0][value]="1234" \
+           credentials[0][temporary]:=false
+
+#  Here we create a user that will have the admin role.
+   echo "Creating UserTasks Admin User"
+   http --ignore-stdin -b -A bearer -a "${KEYCLOAK_ADMIN_ACCESS_TOKEN}" POST "$KEYCLOAK_URL/admin/realms/$REALM_NAME/users" \
+               emailVerified:=true \
+               username="my-admin-user" \
+               email="someotheremailaddress@somedomain.com" \
+               firstName="local-admin" \
+               lastName="dev" \
+               enabled:=true \
+               credentials[0][type]="password" \
+               credentials[0][value]="1234" \
+               credentials[0][temporary]:=false \
+
+   echo "Fetching Users' IDs"
+   NON_ADMIN_USER_ID=$(http --ignore-stdin -b -A bearer -a "${KEYCLOAK_ADMIN_ACCESS_TOKEN}" "$KEYCLOAK_URL/admin/realms/$REALM_NAME/users/?username=my-user" | jq -r ".[0].id")
+   ADMIN_USER_ID=$(http --ignore-stdin -b -A bearer -a "${KEYCLOAK_ADMIN_ACCESS_TOKEN}" "$KEYCLOAK_URL/admin/realms/$REALM_NAME/users/?username=my-admin-user" | jq -r ".[0].id")
+
+   echo "Fetching Roles' IDs"
+   VIEW_USERS_ROLE_ID=$(http --ignore-stdin -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" "$KEYCLOAK_URL/admin/realms/default/ui-ext/available-roles/users/$NON_ADMIN_USER_ID?first=0&max=1&search=view-users" | jq -r ".[0].id")
+   USER_TASKS_ADMIN_ROLE_ID=$(http --ignore-stdin -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" "$KEYCLOAK_URL/admin/realms/$REALM_NAME/roles/lh-user-tasks-admin" | jq -r ".id")
+
+#  Here we assign the view-users role to the nonAdmin user, and subsequently to the admin user as well. The view-users role
+#  allows users to see their userInfo details.
+   echo "Assigning View-Users Role to Non Admin User"
+   http --ignore-stdin -b -A bearer -a "${KEYCLOAK_ADMIN_ACCESS_TOKEN}" POST "$KEYCLOAK_URL/admin/realms/$REALM_NAME/users/$NON_ADMIN_USER_ID/role-mappings/clients/$REAL_MANAGEMENT_CLIENT_ID" \
+              [0][id]="$VIEW_USERS_ROLE_ID" \
+              [0][name]="view-users"
+
+   echo "Assigning View-Users Role to Admin User"
+   http --ignore-stdin -b -A bearer -a "${KEYCLOAK_ADMIN_ACCESS_TOKEN}" POST "$KEYCLOAK_URL/admin/realms/$REALM_NAME/users/$ADMIN_USER_ID/role-mappings/clients/$REAL_MANAGEMENT_CLIENT_ID" \
+              [0][id]="$VIEW_USERS_ROLE_ID" \
+              [0][name]="view-users"
+
+#  Here we assign the admin role to the admin user.
+   echo "Assigning Admin Role to Admin User"
+   http --ignore-stdin -b -A bearer -a "${KEYCLOAK_ADMIN_ACCESS_TOKEN}" POST "$KEYCLOAK_URL/admin/realms/$REALM_NAME/users/$ADMIN_USER_ID/role-mappings/realm" \
+              [0][id]="$USER_TASKS_ADMIN_ROLE_ID" \
+              [0][name]="lh-user-tasks-admin"
+
+   echo "Roles successfully assigned to users!"
+
+#  Here we make the created client public, and also disabled the serviceAccounts. This is done so that users can be properly
+#  authenticated when using the created client's credential when fetching access tokens.
+   echo "Making the client public"
+   http --ignore-stdin -q -A bearer -a "$KEYCLOAK_ADMIN_ACCESS_TOKEN" PUT "$KEYCLOAK_URL/admin/realms/$REALM_NAME/clients/user-tasks-client" \
+             id="$KEYCLOAK_CLIENT_ID"  \
+             enabled:=true \
+             serviceAccountsEnabled:=false \
+             publicClient:=true
 }
 
-configure_keycloak
+#  Here we are allowing the configure_keycloak function to receive an optional param
+configure_keycloak $1
