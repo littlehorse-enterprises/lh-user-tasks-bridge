1ed540c03cc4a30670e5aed43b91bbc9095ee76f
Hazim Arafa <hazimoarafa@gmail.com>
2024-11-18 16:09:04 -0800

    refactor: remove env as tenantId is in the URL now (#69)
    Do not merge this until
    https://github.com/littlehorse-enterprises/lh-user-tasks/pull/18 is
    merged
    
    Edit: 
    
    It is merged now just waiting on approval.

--- standalone/ui-entrypoint.sh
+++ standalone/ui-entrypoint.sh
@@ -14,6 +14,5 @@ export KEYCLOAK_REALM="default"
 export KEYCLOAK_CLIENT_ID="user-tasks-client"
 export KEYCLOAK_CLIENT_SECRET="any-secret"
 export LHUT_API_URL="http://localhost:8089"
-export LHUT_TENANT_ID="default"
 
 node /user-task-ui/ui/server.js
