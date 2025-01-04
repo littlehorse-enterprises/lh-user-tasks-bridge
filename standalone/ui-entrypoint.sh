7b1619a191bde24017bc4e1b765da67c5689f6dc
Hazim Arafa <hazimoarafa@gmail.com>
2025-01-04 12:36:17 -0800

    build(deps): next 14 -> 15 (#79)

--- standalone/ui-entrypoint.sh
+++ standalone/ui-entrypoint.sh
@@ -7,12 +7,12 @@ export PORT=3000
 export HOSTNAME="0.0.0.0"
 
 # app env variables
-export NEXTAUTH_URL="http://localhost:3000"
-export NEXTAUTH_SECRET="any-secret"
-export KEYCLOAK_HOST="http://localhost:8888"
-export KEYCLOAK_REALM="default"
-export KEYCLOAK_CLIENT_ID="user-tasks-client"
-export KEYCLOAK_CLIENT_SECRET="any-secret"
+export AUTH_URL="http://localhost:3000"
+export AUTH_SECRET="any-secret"
+export AUTH_KEYCLOAK_HOST="http://localhost:8888"
+export AUTH_KEYCLOAK_REALM="default"
+export AUTH_KEYCLOAK_CLIENT_ID="user-tasks-client"
+export AUTH_KEYCLOAK_CLIENT_SECRET="any-secret"
 export LHUT_API_URL="http://localhost:8089"
 
 node /user-task-ui/ui/server.js
