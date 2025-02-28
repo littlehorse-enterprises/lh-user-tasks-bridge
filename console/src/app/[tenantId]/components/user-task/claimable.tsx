665b32ea24a1313c635f75ddfef401c6ac19dbe8
Hazim Arafa <hazimoarafa@gmail.com>
2025-02-28 12:28:58 -0800

    feat: add claimable tasks component

---
+++ console/src/app/[tenantId]/components/user-task/claimable.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { UserGroup } from "@littlehorse-enterprises/user-tasks-bridge-api-client";
+
+export default function ClaimableUserTasks({
+	userGroups,
+}: {
+	userGroups: UserGroup[];
+}) {
+		return;
+}
