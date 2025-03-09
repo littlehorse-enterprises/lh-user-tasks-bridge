9cea205a8e8a6afd2f5b95c5338dfc63cbc86a7b
Hazim O. Arafa <hazimoarafa@gmail.com>
2025-03-08 16:17:47 -0800

    feat: metrics (#92)

--- console/next.config.mjs
+++ console/next.config.mjs
@@ -2,5 +2,9 @@
 const nextConfig = {
   output: "standalone",
   reactStrictMode: false,
+  experimental: {
+    instrumentationHook: true,
+    serverComponentsExternalPackages: ["@opentelemetry/instrumentation"],
+  },
 };
 export default nextConfig;
